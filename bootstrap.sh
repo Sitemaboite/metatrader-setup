045cba506208c99f1fa4200a654555ad6769c994
Loïc <loic@piernas.fr>
2025-02-18 16:02:57 +0100

    clear

--- bootstrap.sh
+++ bootstrap.sh
@@ -56,6 +56,7 @@ main() {
 
     echo "Ansible and Git installation completed successfully."
     ansible-pull -U "$GIT_REPOSITORY" bootstrap.yml --ask-become-pass
+    clear
     /opt/metatrader-setup/gh_setup.sh
 }
 
