463b89da85ca41784bb557759561ae02bae74d2b
Loïc <loic@piernas.fr>
2025-03-11 17:44:13 +0100

    update launch

--- launch.sh
+++ launch.sh
@@ -9,12 +9,14 @@ ask_and_run_ansible(){
     echo "1) Git Setup Only (gh_setup.yml)"
     echo "2) Full Installation (setup.yml)"
     echo "3) Time restriction only (time.yml)"
+    echo "4) VNC setup only (vnc.yml)"
     read -p "Enter choice (1, 2 or 3): " choice
 
     case $choice in
         1) PLAYBOOK="gh_setup.yml" ;;
         2) PLAYBOOK="setup.yml" ;;
         3) PLAYBOOK="time_restriction.yml" ;;
+        4) PLAYBOOK="vnc_setup.yml" ;;
         *) echo "Invalid choice" && exit 1 ;;
     esac
     # Prompt for SSH connection details
